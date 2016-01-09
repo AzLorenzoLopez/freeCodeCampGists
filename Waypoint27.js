db5e678a980500191c4d0d96b8a64cc03a764f4f
Lorenzo <az.lorenzo.lopez@gmail.com>
2016-01-09 12:55:07 -0700

    Create Waypoint27.js

---
+++ Waypoint27.js
@@ -0,0 +1,6 @@
+// This is the solution for Waypoint 27 http://www.freecodecamp.com/challenges/waypoint-concatenating-strings-with-plus-operator
+var ourStr = "I come first." + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. "  +  "This is the end.";
